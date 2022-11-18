86d4da9d5ecd0509a0e8db4de14f3034a33cb56b
TechInnovation-Blockchain <newcaesar628@gmail.com>
2022-11-18 15:45:09 -0500

    add Resume

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              <span className="purple"> A Breif Bio </span> 
+            </h1>
+            <p className="home-about-body">
+              My professional technical background below
+              <br />
+              <br />I expertised in
+              <i>
+                <b className="purple"> Javascript and its Frameworks like React JS, NextJS and NodeJS </b>
+              </i>
+              <br />
+              <br />
+              I continuously grow in IT industry by learning  -&nbsp;
+              <i>
+                <b className="purple">New Top-notch Technologies</b> and {" "}
+                <b className="purple">
+                  System Level Engineering.
+                </b>
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also apply my passion for developing products
+              with <b className="purple">Python</b> and
+              <i>
+                <b className="purple">
+                  {" "}
+                  Cloud Computing Services
+                </b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/TechInnovation-Blockchain"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/w-s-ad/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
